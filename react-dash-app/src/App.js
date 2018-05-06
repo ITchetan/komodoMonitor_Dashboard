5bf3fc9865203ad18f73d6bfdaca5a3d441fe062
icebirdmail <37310305+icebirdmail@users.noreply.github.com>
2018-05-07 11:51:03 +1200

    Added React-Bootstrap
    Added files for React-Bootstrap, starting testing.

--- react-dash-app/src/App.js
+++ react-dash-app/src/App.js
@@ -2,19 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+class Header extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div classname="Header">
+      <Jumbotron>
+        <h1>Komodo Monitr</h1>
+        <p>
+          Player information for player-to-be-named-later.
         </p>
-      </div>
-    );
+      </Jumbotron>;
+    )
   }
 }
 
