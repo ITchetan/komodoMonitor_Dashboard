6760b1a186ea721d2cb246c772b786e7c13d471e
icebirdmail <37310305+icebirdmail@users.noreply.github.com>
2018-05-14 09:42:41 +1200

    Update

--- react-dash-app/src/components/Header.js
+++ react-dash-app/src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Panel } from 'react-bootstrap';
+import { Grid, Row, Col, Panel } from 'react-bootstrap';
 
 const Header = () => (
   <div className="Header">
+  <Grid>
+  <Row>
+  <Col>
   <Panel>
     <Panel.Heading>
-    <h1>Komodo Monitr</h1>
+      <h1>Komodo Monitr</h1>
+      <h3>John Doe</h3>
     </Panel.Heading>
   </Panel>
+  </Col>
+  </Row>
+  </Grid>
   </div>
 );
 
