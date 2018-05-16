3a618cca7b155b7b27ec63cbe98a2a9a869ce78c
Amol <37284591+imamolg@users.noreply.github.com>
2018-05-16 12:59:02 +1200

    header
    modified header to show logo and player image

--- react-dash-app/src/components/Header.js
+++ react-dash-app/src/components/Header.js
@@ -2,15 +2,26 @@ import React from 'react';
 import { Container, Row, Col, Card, CardBody, CardHeader} from 'reactstrap';
 
 const Header = () => (
-  <div className="Header">
+  
+  <div className="Header" >
   <Container>
-  <Row>
-  <Col>
-  <Card>
-    <CardHeader>Komodo Monitr</CardHeader>
-  </Card>
-  </Col>
-  </Row>
+        
+      <Row>
+        
+
+        <Col sm={10}>
+            <img src={require('./komodoLogo.png')} alt="Komodo Monitr" />
+                     
+        </Col>
+          
+          <Col sm={2}>
+          <button ><img src={require('./fakePlayer.jpg')} height="100" width="100" /></button>
+
+          </Col>
+          
+      </Row>
+      
+    
   </Container>
   </div>
 );
