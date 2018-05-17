332e8fb3c4c5935c985bb17d74d6e62f132bdf12
icebirdmail <37310305+icebirdmail@users.noreply.github.com>
2018-05-17 14:41:36 +1200

    Updates to Wellness modal
    Added labels to inputs.

--- react-dash-app/src/components/ModalFormWellness.js
+++ react-dash-app/src/components/ModalFormWellness.js
@@ -23,13 +23,41 @@ class ModalFormWellness extends React.Component {
     return (
       <div className = "ModalForm">
       <Container>
-            <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-              <ModalHeader toggle={this.toggle}>Hi{this.props.profileName}, Welcome to wellness survey!!</ModalHeader>
+            <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} size="lg">
+              <ModalHeader toggle={this.toggle}>Hi{this.props.profileName}, please complete your wellness survey!!</ModalHeader>
               <ModalBody>
                 <div>
                       <Row>
-                            <Label for ="Nutrition:" sm = {4} > Nutrition:</Label>
-                            <Col sm={8}>
+                            <Label for ="Nutrition:" sm = {3} > Nutrition:</Label>
+                            <Col sm={2} className="SmallText">
+                              Very Satisfied
+                            </Col>
+                            <Col sm={3}>
+                              <input type="range" list="tickmarks"/>
+                              <datalist id="tickmarks">
+                                <option value="0" label="0%"/>
+                                <option value="10"/>
+                                <option value="20"/>
+                                <option value="30"/>
+                                <option value="40"/>
+                                <option value="50" label="50%"/>
+                                <option value="60"/>
+                                <option value="70"/>
+                                <option value="80"/>
+                                <option value="90"/>
+                                <option value="100" label="100%"/>
+                              </datalist>
+                              </Col>
+                              <Col className="SmallText" sm={3}>
+                              Very malnourished
+                              </Col>
+                      </Row>
+                      <Row>
+                            <Label for ="Energy" sm = {3} >Energy:</Label>
+                            <Col sm={2} className="SmallText">
+                            Full of energy
+                            </Col>
+                            <Col sm={3}>
                             <input type="range" list="tickmarks"/>
                               <datalist id="tickmarks">
                                 <option value="0" label="0%"/>
@@ -45,10 +73,16 @@ class ModalFormWellness extends React.Component {
                                 <option value="100" label="100%"/>
                               </datalist>
                             </Col>
+                            <Col sm={3} className="SmallText">
+                            Very fatigued
+                            </Col>
                       </Row>
                       <Row>
-                            <Label for ="Energy" sm = {4} >Energy:</Label>
-                            <Col sm={8}>
+                            <Label for ="Sleep quality" sm = {3} >Sleep quality:</Label>
+                            <Col sm={2} className="SmallText">
+                              Very Rested
+                            </Col>
+                            <Col sm={3}>
                             <input type="range" list="tickmarks"/>
                               <datalist id="tickmarks">
                                 <option value="0" label="0%"/>
@@ -64,10 +98,16 @@ class ModalFormWellness extends React.Component {
                                 <option value="100" label="100%"/>
                               </datalist>
                             </Col>
+                            <Col sm={3} className="SmallText">
+                              Unable to sleep
+                            </Col>
                       </Row>
                       <Row>
-                            <Label for ="Sleep quality" sm = {4} >Sleep quality:</Label>
-                            <Col sm={8}>
+                            <Label for ="Stress" sm = {3} >Stress:</Label>
+                            <Col sm={2} className="SmallText">
+                              Very relaxed
+                            </Col>
+                            <Col sm={3}>
                             <input type="range" list="tickmarks"/>
                               <datalist id="tickmarks">
                                 <option value="0" label="0%"/>
@@ -83,10 +123,16 @@ class ModalFormWellness extends React.Component {
                                 <option value="100" label="100%"/>
                               </datalist>
                             </Col>
+                            <Col sm={3} className="SmallText">
+                              Highly stressed
+                            </Col>
                       </Row>
                       <Row>
-                            <Label for ="Stress" sm = {4} >Stress:</Label>
-                            <Col sm={8}>
+                            <Label for ="Sleep amount" sm = {3} >Sleep amount:</Label>
+                            <Col sm={2} className="SmallText">
+                              Less than 6 hours
+                            </Col>
+                            <Col sm={3}>
                             <input type="range" list="tickmarks"/>
                               <datalist id="tickmarks">
                                 <option value="0" label="0%"/>
@@ -102,10 +148,16 @@ class ModalFormWellness extends React.Component {
                                 <option value="100" label="100%"/>
                               </datalist>
                             </Col>
+                            <Col sm={3} className="SmallText">
+                              More than 9 hours
+                            </Col>
                       </Row>
                       <Row>
-                            <Label for ="Sleep amount" sm = {4} >Sleep amount:</Label>
-                            <Col sm={8}>
+                            <Label for ="Muscle soreness" sm = {3} >Muscle pain:</Label>
+                            <Col sm={2} className="SmallText">
+                              Feeling Great
+                            </Col>
+                            <Col sm={3}>
                             <input type="range" list="tickmarks"/>
                               <datalist id="tickmarks">
                                 <option value="0" label="0%"/>
@@ -121,24 +173,8 @@ class ModalFormWellness extends React.Component {
                                 <option value="100" label="100%"/>
                               </datalist>
                             </Col>
-                      </Row>
-                      <Row>
-                            <Label for ="Muscle pain" sm = {4} >Muscle pain:</Label>
-                            <Col sm={8}>
-                            <input type="range" list="tickmarks"/>
-                              <datalist id="tickmarks">
-                                <option value="0" label="0%"/>
-                                <option value="10"/>
-                                <option value="20"/>
-                                <option value="30"/>
-                                <option value="40"/>
-                                <option value="50" label="50%"/>
-                                <option value="60"/>
-                                <option value="70"/>
-                                <option value="80"/>
-                                <option value="90"/>
-                                <option value="100" label="100%"/>
-                              </datalist>
+                            <Col sm={3} className="SmallText">
+                              Very sore
                             </Col>
                       </Row>
                 </div>
