00a1ae8db92ea1fa84e369d9a5d37ae0abb57f80
Carltisan <33442250+Carltisan@users.noreply.github.com>
2018-05-28 14:02:39 +1200

    updated login page flow

--- react-dash-app/src/components/Loading.js
+++ react-dash-app/src/components/Loading.js
@@ -3,13 +3,6 @@ import { Container, Row, Col, Button, Form, FormGroup, Label, Input } from 'reac
 import ReactLoading from 'react-loading';
 import Login from './Login';
 
-const styles = {
-    paperContainer: {
-        height:600 ,
-        backgroundImage: `url(${"./field-bg.59a5e231.jpg"})`
-    }
-};
-
 class Loading extends Component {
   constructor(props){
     super(props);
@@ -145,50 +138,7 @@ class Loading extends Component {
 render(){
 
   return(
-    <div className="Login" style={styles.paperContainer}>
-    <Container>
-      <Row>
-      <Col sm={10}>
-          <img src={require('./komodo.png')} alt="Komodo Monitr" height="50" />
-          <span className="BrandName">KOMODO </span><span className="SubBrand">MONITR</span>
-
-      </Col>
-      </Row>
-      <Row>
-      <Col className="text-center">
-      <h5>Welcome to Komodo Monitr, please Log in</h5>
-      </Col>
-      </Row>
-      <Row>
-      <Col className="text-center">
-              <p>Email</p>
-              <input type="text"
-                     readOnly
-                     id="theInput"
-                     value={this.props.email}
-                      />
-                <p>Password</p>
-               <input type="text"
-                      readOnly
-                      id="theInput2"
-                      value={this.props.pass}
-                       />
-                       <p/>
-                  <input type="submit" disabled/>
-                  <button disabled>
-                  Default
-                  </button>
-              </Col>
-        </Row>
-        <Row>
-          <Col xs="6">
-          </Col>
-          <Col>
-          <ReactLoading type='spin' color='#d40000'/>
-          </Col>
-          </Row>
-      </Container>
-    </div>
+  <Login isLoading={true}/>
   )
 }
 }
