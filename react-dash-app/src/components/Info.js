1044974f91cdafdffb1b430a3b2b6ded198040dc
icebirdmail <37310305+icebirdmail@users.noreply.github.com>
2018-06-01 09:35:49 +1200

    Format changes to info pane.

--- react-dash-app/src/components/Info.js
+++ react-dash-app/src/components/Info.js
@@ -9,51 +9,50 @@ class Info extends Component{
 
     return (
       <div >
-        <h4>Information</h4>
+        <h3>Information</h3>
         <hr />
         <Row >
-        <Col sm={5} >
-        <div className="info green"></div>
-        </Col>
-        <Col sm={7} className="d-flex justify-content-between">
-        <h5>Optimal.</h5>
-        </Col>
+          <Col xs={3} >
+            <div className="info green"></div>
+          </Col>
+          <Col xs={9} className="d-flex">
+            <p className='text-left'>You are in your optimal training zone.</p>
+          </Col>
         </Row>
         <hr />
         <Row>
-        <Col sm={5} >
-        <div className="info orange"></div>
-        </Col>
-        <Col sm={7} className="d-flex justify-content-between">
-        <h5>Be cautious. </h5>
-        </Col>
-        </Row>
-        <hr />
-
-        <Row >
-        <Col sm={5} >
-        <div className="info red"></div>
-        </Col>
-        <Col sm={7} className="d-flex justify-content-between">
-        <h5>Warning! Increased risk of injury.</h5>
-        </Col>
+          <Col xs={3} >
+            <div className="info orange"></div>
+          </Col>
+          <Col xs={9} className="d-flex">
+            <p className='text-left'>Be cautious. You may be increasing your risk of injury and poor performance.</p>
+          </Col>
         </Row>
         <hr />
         <Row >
-        <Col sm={5} >
-        <div className="info blue"></div>
-        </Col>
-        <Col sm={7} className="d-flex justify-content-between">
-        <h5>Below optimal.</h5>
-        </Col>
+          <Col xs={3} >
+            <div className="info red"></div>
+          </Col>
+          <Col xs={9} className="d-flex">
+            <p className="text-left">Warning! You are at increased risk of injury.</p>
+          </Col>
         </Row>
         <hr />
         <Row >
-        <Col sm={5} className="d-flex justify-content-between">
+          <Col xs={3}>
+            <div className="info blue"></div>
+          </Col>
+          <Col xs={9} className="d-flex">
+            <p className="text-left">Below optimal. You can work harder without risking injury, and may improve your performance.</p>
+          </Col>
+        </Row>
+        <hr />
+        <Row >
+        <Col xs={3} className="d-flex">
         <h5><strong>Chart filter</strong></h5>
         </Col>
-        <Col sm={7} className="d-flex justify-content-between">
-        <h5>Will show selected range if available.</h5>
+        <Col xs={9} className="d-flex">
+        <p className="text-left">Will show selected range if available.</p>
         </Col>
         </Row>
         <hr />
