fc71916307a3a3ad8ce572564b0f9d8d256cc10b
icebirdmail <37310305+icebirdmail@users.noreply.github.com>
2018-05-31 13:42:41 +1200

    Fixed mismatching player names

--- react-dash-app/src/App.js
+++ react-dash-app/src/App.js
@@ -588,7 +588,7 @@ console.log(wellnessWeeklyTotal)
       renderModal() {
             let rpeform = []
             for (let i = 0; i < this.state.playerSessionId.length; i++){
-              rpeform.push( <div key={this.state.playerSessionId[i]}> <ModalFormRPE loginToken={this.state.loginToken} profileName ={this.state.playerFirstData[2]} playerSessionId={this.state.playerSessionId[i]} playerSessionDate = {this.state.playerSessionDate[i]}/> </div> )
+              rpeform.push( <div key={this.state.playerSessionId[i]}> <ModalFormRPE loginToken={this.state.loginToken} profileName ={this.state.playerFirstData[0]} playerSessionId={this.state.playerSessionId[i]} playerSessionDate = {this.state.playerSessionDate[i]}/> </div> )
             }
             return rpeform
           }
@@ -691,7 +691,7 @@ console.log(wellnessWeeklyTotal)
             {this.renderModal()}
 
             {this.state.wellnessForm === true &&
-            <ModalFormWellness loginToken={this.state.loginToken} profileName = {this.state.playerFirstData[2]}/>
+            <ModalFormWellness loginToken={this.state.loginToken} profileName = {this.state.playerFirstData[0]}/>
             }
 
 
